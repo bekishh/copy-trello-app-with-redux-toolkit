1c191f59a5b537d6ac34a577c6906c0271f880e9
Beknazar <beknazarzholdoshbekov1@gmail.com>
2023-09-06 01:11:22 +0600

    added browser router

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { SnackbarProvider } from "notistack";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SnackbarProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </SnackbarProvider>
+    <BrowserRouter>
+      <SnackbarProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </SnackbarProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
